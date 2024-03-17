82e0b26909889a5a15a01d7abfb4e275ebce5c3d
DESKTOP-MNOT85T\01ki0 <01ki07@naver.com>
2024-03-17 21:57:52 +0900

    /test/user-loan-info 테스트 api 추가

--- src/main/java/com/side/freedomdaybackend/domain/TestController.java
+++ src/main/java/com/side/freedomdaybackend/domain/TestController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
@@ -32,12 +35,74 @@ public class TestController {
         return new ApiResponse<ArrayList<TestDto>>(list);
     }
 
+    @GetMapping("/user-loan-info")
+    public ApiResponse userLoanInfo() {
+        List<LoanDto> loanDtoList = new ArrayList<>();
+        loanDtoList.add(
+                new LoanDto(
+                        "토스뱅크 신용대출",
+                        "생활비",
+                        16,
+                        19886317,
+                        LocalDateTime.of(2024, Month.DECEMBER, 04, 0, 0),
+                        15));
+        loanDtoList.add(
+                new LoanDto(
+                        "카카오뱅크 신용대출",
+                        "생활비",
+                        16,
+                        19886317,
+                        LocalDateTime.of(2024, Month.DECEMBER, 04, 0, 0),
+                        15));
+
+        UserLoanDto userLoanDto = new UserLoanDto("이문기", 5800000, 90000000, 2, loanDtoList);
+        return new ApiResponse(userLoanDto);
+
+    }
+
     @GetMapping("/error")
     public void errorTest() {
         String message = "테스트 성공";
         throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 
+
+}
+
+
+/**
+ * 이름 (홍길동)
+ * 지난달 총 납부액 숫자로
+ * 총 납부해야 할 금액
+ * 나의 대출 개수
+ * details관련 내용
+     * 나의대출1(토스뱅크 신용대출)
+     * 용도(생활비)
+     * 납부일까지 (D-16) -- 숫자로?
+     * 나의대출금액1(19,886,317)
+     * 만기일(2024.12.04)
+     * 납부일(매달 15일)
+ */
+@Data
+@AllArgsConstructor
+class UserLoanDto {
+    String userName; // 유저 이름
+    long previousMonthPayment; // 지난달 납부액
+    long totalPayment; // 총 납부 금액
+    int loanCount; // 대출 개수
+    List<LoanDto> loanDtoList;
+
+}
+
+@Data
+@AllArgsConstructor
+class LoanDto {
+    String loanName; // 대출이름
+    String purpose; // 용도
+    long paymentDDay; // 남은 납부일
+    int loanAmount; // 대출 금액
+    LocalDateTime loanExpirationDate;// 만기일
+    int paymentDueDay;// 납부일
 }
 
 @Data
