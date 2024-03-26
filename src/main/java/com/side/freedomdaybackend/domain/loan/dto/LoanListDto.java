2c6e542f755b6138fc3a773938fa977453f259e0
MoonGi <01ki07@naver.com>
2024-03-26 16:16:26 +0900

    에러 수정

--- src/main/java/com/side/freedomdaybackend/domain/loan/dto/LoanListDto.java
+++ src/main/java/com/side/freedomdaybackend/domain/loan/dto/LoanListDto.java
@@ -17,7 +17,7 @@ public class LoanListDto {
     private String bankCode; // 은행
     private Long totalPrincipal; // 총 원금
     private Long repaymentAmount; // 상환 완료 금액
-    private String interestRate; // 연 이자율
+    private int interestRate; // 연 이자율
     private int loanPeriod; // 연 이자율
     private Boolean variableRate; // 변동금리 여부
     private LocalDateTime originationDate; // 대출 기간  TODO) 기간에 대한 정보를 어떻게 저장할지 결졍해야함
