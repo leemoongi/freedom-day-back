833ab7720d49bf3b9d61e11043c390a6c5c60450
MoonGi <01ki07@naver.com>
2024-04-15 14:50:17 +0900

    start.sh 에러로그, 어플로그 제거

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,8 @@
 PROJECT_ROOT="/home/ec2-user/freedom-day-backend"
 JAR_FILE="$PROJECT_ROOT/freedom-day-backend-0.0.1-SNAPSHOT.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
+#APP_LOG="$PROJECT_ROOT/application.log"
+#ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
@@ -11,7 +11,7 @@ source ~/.bash_profile
 
 echo "$TIME_NOW > $JAR_FILE --spring.profiles.active=dev 실행" >> $DEPLOY_LOG
 echo "java -jar $JAR_FILE --spring.profiles.active=dev"
-nohup java -jar $JAR_FILE --spring.profiles.active=dev > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE --spring.profiles.active=dev &
 
 # 프로세스 아이디가 안나올때
 sleep 3
