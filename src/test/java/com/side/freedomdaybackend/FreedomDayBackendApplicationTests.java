f3c1ce6304a50db691c335cec4366ca9d0f00570
MoonGi <01ki07@naver.com>
2024-04-19 12:35:40 +0900

    빌드 테스트

--- src/test/java/com/side/freedomdaybackend/FreedomDayBackendApplicationTests.java
+++ src/test/java/com/side/freedomdaybackend/FreedomDayBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.side.freedomdaybackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FreedomDayBackendApplicationTests {
 
 	@Test
