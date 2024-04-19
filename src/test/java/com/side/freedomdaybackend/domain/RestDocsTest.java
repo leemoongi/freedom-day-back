b85dad335f1234a16d3c74223a484f0bacce3c74
MoonGi <01ki07@naver.com>
2024-04-19 12:30:27 +0900

    빌드 테스트

--- src/test/java/com/side/freedomdaybackend/domain/RestDocsTest.java
+++ src/test/java/com/side/freedomdaybackend/domain/RestDocsTest.java
@@ -1,42 +1,42 @@
-package com.side.freedomdaybackend.domain;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs // SpringRestDocs 자동 설정
-@SpringBootTest // 모든 빈 사용
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-public abstract class RestDocsTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation)
-                        .uris()
-                        .withScheme("https")
-                        .withHost("freedom-day.site/api")
-                        .withPort(443))
-                .addFilter(new CharacterEncodingFilter("UTF-8", true))
-                .alwaysDo(print()) // 항상 프린트
-                .build();
-
-    }
-}
+//package com.side.freedomdaybackend.domain;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.restdocs.RestDocumentationContextProvider;
+//import org.springframework.restdocs.RestDocumentationExtension;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//import org.springframework.web.filter.CharacterEncodingFilter;
+//
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//
+//@AutoConfigureMockMvc
+//@AutoConfigureRestDocs // SpringRestDocs 자동 설정
+//@SpringBootTest // 모든 빈 사용
+//@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+//public abstract class RestDocsTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @BeforeEach
+//    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation)
+//                        .uris()
+//                        .withScheme("https")
+//                        .withHost("freedom-day")
+//                        .withPort(443))
+//                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+//                .alwaysDo(print()) // 항상 프린트
+//                .build();
+//
+//    }
+//}
