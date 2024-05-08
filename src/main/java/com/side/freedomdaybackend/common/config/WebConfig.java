500d466a46597ee4275604cde2501cebcc5d67c3
MoonGi <01ki07@naver.com>
2024-05-08 15:49:48 +0900

    cors 수정

--- src/main/java/com/side/freedomdaybackend/common/config/WebConfig.java
+++ src/main/java/com/side/freedomdaybackend/common/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
